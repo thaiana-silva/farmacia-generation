3d717cee9e94dd5482382e74093895e2d97839bc
Thaiana Silva <thaianacelia@gmail.com>
2024-02-28 13:28:01 -0300

    alteração footer e conclusão do projeto

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import {
-  FacebookLogo,
-  InstagramLogo,
-  LinkedinLogo,
-} from "@phosphor-icons/react";
+import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
 
 function Footer() {
   return (
     <>
-      <div className="flex justify-center bg-indigo-900 text-white">
+      <div className="flex justify-center bg-blue-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">Farmácia Generation | Copyright: </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <InstagramLogo size={48} weight="bold" />
-            <FacebookLogo size={48} weight="bold" />
+            <a
+              href="https://www.linkedin.com/in/thaiana-silva/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              <LinkedinLogo size={48} weight="bold" />
+            </a>
+            <a
+              href="https://github.com/thaiana-silva/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              <GithubLogo size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
